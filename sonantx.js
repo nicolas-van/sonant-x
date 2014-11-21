232d500ed7114e8b6980364b090f5046acb0435b
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2014-11-21 10:42:54 +0100

    Potential bug in audio generation

--- sonantx.js
+++ sonantx.js
@@ -189,7 +189,6 @@ sonantx.AudioGenerator.prototype.getAudioBuffer = function(callBack) {
     var mixBuf = this.mixBuf;
     var waveSize = this.waveSize;
 
-    var waveBytes = waveSize * WAVE_CHAN * 2;
     var buffer = audioCtx.createBuffer(WAVE_CHAN, this.waveSize, WAVE_SPS); // Create Mono Source Buffer from Raw Binary
     var lchan = buffer.getChannelData(0);
     var rchan = buffer.getChannelData(1);
@@ -197,7 +196,7 @@ sonantx.AudioGenerator.prototype.getAudioBuffer = function(callBack) {
     var iterate = function() {
         var beginning = new Date();
         var count = 0;
-        while (b < (waveBytes / 2)) {
+        while (b < waveSize) {
             var y = 4 * (mixBuf[b * 4] + (mixBuf[(b * 4) + 1] << 8) - 32768);
             y = y < -32768 ? -32768 : (y > 32767 ? 32767 : y);
             lchan[b] = y / 32768;

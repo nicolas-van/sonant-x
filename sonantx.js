36f1248060423ea89d640b0d8d859f83b18e71f7
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2020-08-31 09:29:55 +0200

    wip

--- sonantx.js
+++ sonantx.js
@@ -1,38 +1,12 @@
-//
-// Sonant-X
-//
-// Copyright (c) 2014 Nicolas Vanhoren
-//
-// Sonant-X is a fork of js-sonant by Marcus Geelnard and Jake Taylor. It is
-// still published using the same license (zlib license, see below).
-//
-// Copyright (c) 2011 Marcus Geelnard
-// Copyright (c) 2008-2009 Jake Taylor
-//
-// This software is provided 'as-is', without any express or implied
-// warranty. In no event will the authors be held liable for any damages
-// arising from the use of this software.
-//
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-//
-// 1. The origin of this software must not be misrepresented; you must not
-//    claim that you wrote the original software. If you use this software
-//    in a product, an acknowledgment in the product documentation would be
-//    appreciated but is not required.
-//
-// 2. Altered source versions must be plainly marked as such, and must not be
-//    misrepresented as being the original software.
-//
-// 3. This notice may not be removed or altered from any source
-//    distribution.
+"use strict"
+
+window.sonantx = {}
+
+;(() => {
 
 const WAVE_SPS = 44100 // Samples per second
 const WAVE_CHAN = 2 // Channels
 const MAX_TIME = 33 // maximum time, in millis, that the generator can use consecutively
-
-var WAVE_SPS = 44100;
 
 // Oscillators
 function osc_sin (value) {
@@ -273,4 +247,4 @@ sonantx.MusicGenerator.prototype.connect = function(target) {
     this.chain[this.chain.length - 1].connect(target);
 };
 
-})();
+})()

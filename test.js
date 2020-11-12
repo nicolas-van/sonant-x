f816647c97a5f00d107633f1a2e663e9b9d1d4be
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2020-11-12 12:08:00 +0100

    Added sample showing how to convert audio buffer to audio

--- test.js
+++ test.js
@@ -2,6 +2,8 @@
 import * as sonantx from './sonantx'
 import song from './example-songs/4k-synth'
 import 'regenerator-runtime/runtime'
+import audioBufferToWav from 'audiobuffer-to-wav'
+import { base64 } from 'rfc4648'
 
 console.log('initing app')
 
@@ -77,5 +79,50 @@ document.querySelector('button.ting').addEventListener('click', () => {
     audioBufferSource.buffer = buf
     audioBufferSource.connect(audioCtx().destination)
     audioBufferSource.start()
+  })
+})
+
+document.querySelector('button.audioting').addEventListener('click', () => {
+  console.log('audioting')
+  const instr = {
+    osc1_oct: 9,
+    osc1_det: 0,
+    osc1_detune: 0,
+    osc1_xenv: 0,
+    osc1_vol: 161,
+    osc1_waveform: 0,
+    osc2_oct: 9,
+    osc2_det: 0,
+    osc2_detune: 4,
+    osc2_xenv: 0,
+    osc2_vol: 182,
+    osc2_waveform: 0,
+    noise_fader: 0,
+    env_attack: 100,
+    env_sustain: 1818,
+    env_release: 18181,
+    env_master: 192,
+    fx_filter: 0,
+    fx_freq: 0,
+    fx_resonance: 254,
+    fx_delay_time: 6,
+    fx_delay_amt: 108,
+    fx_pan_freq: 3,
+    fx_pan_amt: 61,
+    lfo_osc1_freq: 0,
+    lfo_fx_freq: 0,
+    lfo_freq: 3,
+    lfo_amt: 94,
+    lfo_waveform: 2
+  }
+  sonantx.generateSound(instr, 67, audioCtx().sampleRate, 118).then((buf) => {
+    console.log('finished buffer generation')
+    const wavBuffer = audioBufferToWav(buf)
+    const array = new Uint8Array(wavBuffer)
+    const uri = 'data:audio/wav;base64,' + base64.stringify(array)
+    const audio = new Audio(uri)
+    console.log(uri)
+    console.log('finished audio generation', { wavBuffer, array, uri })
+    audio.play()
   })
 })
